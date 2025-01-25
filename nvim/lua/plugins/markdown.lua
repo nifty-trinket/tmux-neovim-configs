18463eb0eb9f4acca158f0597683dc2ebd624480
nifty-trinket <ewdevenney@gmail.com>
2025-01-24 20:30:21 -0500

    Add files via upload

---
+++ nvim/lua/plugins/markdown.lua
@@ -0,0 +1,11 @@
+{
+	'jxnblk/vim-mdx-js',
+	{
+		"instant-markdown/vim-instant-markdown",
+		ft = { "markdown" },
+		build = "yarn install",
+		config = function()
+			vim.g.instant_markdown_autostart = 0
+		end,
+	}
+}
